5f746eaa6c0f1259660937eb0a528a83f5ee5b95
kashisshgoyal-17 <kashisshgoyal17@gmail.com>
2026-06-18 13:21:16 +0530

    Work of 18/jun2/2026

---
+++ c_program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int rows,totalrows;
+    printf("enter the no of rows");
+    scanf("%d",&rows);
+    totalrows=2*rows;
+    for(i=1;i<total rows;i++)
+    {
+        for(j=1;j<total rows;j++)
+        {
+            prinf(" ");
+        }
+        for(j=1;j<2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
