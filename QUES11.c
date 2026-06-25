8b588aeefb31efde43f34f7676cc75f5a3d9b1cc
kashisshgoyal-17 <kashisshgoyal17@gmail.com>
2026-06-25 10:29:38 +0530

    Add files via upload

---
+++ QUES11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int totalDays, years, weeks, days;
+
+    printf("Enter total number of days: ");
+    scanf("%d", &totalDays);
+
+    years = totalDays / 365;
+    weeks = (totalDays % 365) / 7;
+    days = (totalDays % 365) % 7;
+
+    printf("%d years, %d weeks, %d days\n", years, weeks, days);
+    return 0;
+}
